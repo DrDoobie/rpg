c997432b40d1493602755e37079569c228e76869
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2019-10-25 00:17:21 +0200

    stat bars

---
+++ rpg/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public float health, stamina, mana;
+    [HideInInspector] public float maxHealth = 100.0f, maxStamina = 100.0f, maxMana = 10.0f;
+}
---
+++ rpg/Assets/Scripts/StatBar.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatBar : MonoBehaviour
+{
+    public bool health, stamina, mana;
+    public Text text;
+    private PlayerStats playerStats;
+    private Slider slider;
+
+    private void Start () {
+        playerStats = FindObjectOfType<PlayerStats>();
+        slider = GetComponent<Slider>();
+    }
+
+    private void Update () {
+        StatController();
+    }
+
+    private void StatController () {
+        if(health)
+        {
+            slider.value = playerStats.health;
+            slider.maxValue = playerStats.maxHealth;
+
+            text.text = playerStats.health.ToString("0") + "/" + slider.maxValue;
+
+            return;
+        }
+
+        if(stamina)
+        {
+            slider.value = playerStats.stamina;
+            slider.maxValue = playerStats.maxStamina;
+
+            text.text = playerStats.stamina.ToString("0") + "/" + slider.maxValue;
+
+            return;
+        }
+
+        if(mana)
+        {
+            slider.value = playerStats.mana;
+            slider.maxValue = playerStats.maxMana;
+
+            text.text = playerStats.mana.ToString("0") + "/" + slider.maxValue;
+        }
+    }
+}
