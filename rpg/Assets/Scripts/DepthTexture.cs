c4ea8c7c1b0cdef653e0cdec86a37e3dc188e51b
Wilhelm <w.borwegen@gmail.com>
2019-10-24 14:53:54 +0200

    added water prefab

---
+++ rpg/Assets/Scripts/DepthTexture.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class DepthTexture : MonoBehaviour
+{
+    private Camera mainCam;
+
+    void Start () {
+        mainCam = GetComponent<Camera>();
+        mainCam.depthTextureMode = DepthTextureMode.Depth;
+    }
+}
