e868de18025c6a815108e20df1bebacd694e1c72
Wilhelm <w.borwegen@gmail.com>
2019-10-25 23:33:00 +0200

    started on ai

---
+++ rpg/Assets/Scripts/AI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    GameObject player;
+
+    private void Awake () {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void Update () {
+        MovementController();
+    }
+
+    private void MovementController () {
+        agent.SetDestination(player.transform.position);
+    }
+}
