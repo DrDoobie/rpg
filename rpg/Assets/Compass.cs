e99f96d0c5ccdd291566313749e8e241ff40dd7e
Wilhelm <w.borwegen@gmail.com>
2019-10-25 15:54:28 +0200

    pixel perfect text and mountains

---
+++ rpg/Assets/Compass.cs
@@ -0,0 +1,38 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Compass : MonoBehaviour
+{
+    public bool isEnabled;
+    private Transform player;
+    private Vector3 dir; 
+
+    private void Start () {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void Update () {
+        if(Input.GetButtonDown("Compass"))
+        {
+            isEnabled = !isEnabled;
+        }
+
+        Controller();
+    }
+
+    private void Controller () {
+        if(isEnabled)
+        {
+            GetComponent<Image>().enabled = true;
+
+            dir.z = player.eulerAngles.y;
+            transform.localEulerAngles = dir;
+
+            return;
+        }
+
+        GetComponent<Image>().enabled = false;
+    }
+}
