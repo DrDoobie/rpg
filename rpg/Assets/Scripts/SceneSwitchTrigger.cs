0a0e13bc089b53a227166ad4606ba4e337254dd1
Wilhelm <w.borwegen@gmail.com>
2019-11-02 00:41:27 +0100

    started on scene fade

--- rpg/Assets/Scripts/SceneSwitchTrigger.cs
+++ rpg/Assets/Scripts/SceneSwitchTrigger.cs
@@ -1,15 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneSwitchTrigger : MonoBehaviour
 {
     public string scene;
+    public Image fadeScreen;
+    public Animator anim;
 
     private void OnTriggerEnter (Collider other) {
         if(other.tag == "Player")
         {
-            SavePlayerData();
-            SceneManager.LoadScene(scene);
+            StartCoroutine(Fading()); 
         }
     }
 
@@ -17,5 +20,19 @@ public class SceneSwitchTrigger : MonoBehaviour
         GameController.instance.health = FindObjectOfType<PlayerStats>().health;
         GameController.instance.stamina = FindObjectOfType<PlayerStats>().stamina;
         GameController.instance.mana = FindObjectOfType<PlayerStats>().mana;
+    }
+
+    private void SwitchScene () {
+        SavePlayerData();
+        SceneManager.LoadScene(scene);
+    }
+
+    IEnumerator Fading() 
+    {
+        anim.SetBool("Fade", true);
+
+        yield return new WaitUntil(() => fadeScreen.color.a == 1);
+
+        SwitchScene();
     }
 }
